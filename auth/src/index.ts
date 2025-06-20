4bcbd960b69e5ef3a4f549ad2bc68625469437b1
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-06-20 10:36:56 +0300

    refactor for test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,6 @@
-import express from 'express';
-import morgan from 'morgan';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { currentUserRouter } from './routes/current-user';
-import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import errorHandler from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 
-const app = express();
-
-app.set('trust proxy', true); // Trust the reverse proxy (nginx)
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false, // No encryption
-    secure: true, // Only use cookies over HTTPS
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-// unhandled routes
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
